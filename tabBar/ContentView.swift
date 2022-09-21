5edc51525aeac0f768df5bad89bb6dfdf99b4699
Utku Enes Alagöz <utkuenesalagoz@icloud.com>
2022-09-21 17:03:57 +0300

    Initial Commit

---
+++ tabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tabBar
+//
+//  Created by utku enes alagöz on 21.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tabBar/tabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabBarApp.swift
+//  tabBar
+//
+//  Created by utku enes alagöz on 21.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct tabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
